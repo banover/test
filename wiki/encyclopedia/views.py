b85001bb6bdf8799f9d1ab0c11a6567ee51cf399
Park JaeSoo <qkrwotn5713@naver.com>
2022-08-18 23:39:59 +0900

    Index Page All Clear! + changing markdown file to be read

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-
 from . import util
-
+import markdown
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -21,14 +20,16 @@ def entry(request, entry):
                 break
 
         # Assigning entries[i] to input entry
+        contents = None
         if not key == None:
             input_entry = entries[key]
-        else:
+            contents = markdown.markdown(util.get_entry(input_entry))
+        else:        
             input_entry = None
 
-        # Rendering Entry page
+        # Rendering Entry page        
         return render(request, "encyclopedia/entry.html", {
             #"entries": util.list_entries()                   
             "input_entry": input_entry,
-            "contents": util.get_entry(input_entry)
+            "contents": contents   #util.get_entry(input_entry)
         })
